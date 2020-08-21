e114f8adfe6d80ce7ac70a0e9bc7932945444662
Paul Benavides <pebenavides@uce.edu.ec>
2020-08-20 20:17:02 -0500

    first_commit
    Descarguense el github desktop y clonen la carpeta la version de unity es 2019.3.15f1

---
+++ Recorrido_virtual_biblioteca_UCE/Assets/MouseLook.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+
+    public float mousesensivity=100f;
+
+    public Transform playerbody;
+
+    public float maxrotx,minrotx=0f;
+
+
+    float xrotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")* mousesensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y")* mousesensivity * Time.deltaTime ;
+
+
+        xrotation -= mouseY;
+
+        xrotation = Mathf.Clamp(xrotation, minrotx, maxrotx );
+        transform.localRotation = Quaternion.Euler(xrotation, 0, 0);
+
+        playerbody.Rotate(Vector3.up * mouseX);
+        
+
+
+    }
+
+
+
+
+}
